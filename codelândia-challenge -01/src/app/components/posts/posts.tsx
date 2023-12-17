1f56d115cd43fba397310daf51adccff01bcddc1
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2023-12-17 19:12:11 -0300

    Update posts.tsx

--- codelândia-challenge -01/src/app/components/posts/posts.tsx
+++ codelândia-challenge -01/src/app/components/posts/posts.tsx
@@ -1,15 +1,42 @@
-export function Posts() {
+"use client";
+import { useState } from "react";
+import { ImageComponent } from "@/app/tools/Image";
+import heart from "@/../public/image/icon-heart.png";
+import heartActive from "@/../public/image/icon-heart-active.png";
+
+interface PostProps {
+  data: string;
+  title: string;
+  conteudo: string;
+}
+
+export function Posts({ data, title, conteudo }: PostProps) {
+  const [Favorita, UseFavorita] = useState(heart);
+
+  function Favoritar() {
+    if (Favorita === heart) {
+      return UseFavorita(heartActive);
+    }
+    return UseFavorita(heart);
+  }
+
   return (
-    <div className=" border border-textoPrincipal w-3/4 h-60  pt-10 pb-10 p-5 m-10 flex flex-col justify-between   ">
-      <span className="text-textoPrincipal">17 de ago, 2024</span>
+    <div className=" border border-textoPrincipal w-3/4 h-auto min-h-60    pt-10 pb-10 p-5 m-10 flex flex-col justify-between">
+      <div className="flex justify-between">
+        <span className="text-textoPrincipal">{data}</span>
+        <button onClick={() => Favoritar()}>
+          <ImageComponent
+            src={Favorita}
+            width={32}
+            height={32}
+            alt="favoritar"
+          />
+        </button>
+      </div>
 
-      <h1>O que é linguagem de programação? Conheça as principais</h1>
+      <h1>{title}</h1>
 
-      <p>
-        Uma das mais populares vertentes da tecnologia da informação, a área de
-        programação segue tendo muita demanda de trabalho justamente pela
-        velocidade com que dispositivos tecnológicos vêm avançando.
-      </p>
+      <p className="pt-5">{conteudo}</p>
     </div>
   );
 }
