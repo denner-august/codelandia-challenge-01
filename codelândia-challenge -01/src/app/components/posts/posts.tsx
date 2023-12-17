b01cefa2c8d400b7a714923742071398b63d8cdd
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2023-12-16 22:21:57 -0300

    Create posts.tsx

---
+++ codelândia-challenge -01/src/app/components/posts/posts.tsx
@@ -0,0 +1,15 @@
+export function Posts() {
+  return (
+    <div className=" border border-textoPrincipal w-3/4 h-60  pt-10 pb-10 p-5 m-10 flex flex-col justify-between   ">
+      <span className="text-textoPrincipal">17 de ago, 2024</span>
+
+      <h1>O que é linguagem de programação? Conheça as principais</h1>
+
+      <p>
+        Uma das mais populares vertentes da tecnologia da informação, a área de
+        programação segue tendo muita demanda de trabalho justamente pela
+        velocidade com que dispositivos tecnológicos vêm avançando.
+      </p>
+    </div>
+  );
+}
