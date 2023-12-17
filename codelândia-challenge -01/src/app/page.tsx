7358ecbf1be25933fd8e3f04b44463e67781714e
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2023-12-16 22:21:51 -0300

    Update page.tsx

--- codelândia-challenge -01/src/app/page.tsx
+++ codelândia-challenge -01/src/app/page.tsx
@@ -1,17 +1,21 @@
 import Header_layout from "./components/header/header_component/header_layout";
 
 import { SearchBar } from "./components/header/search_component/search";
+import { Title } from "./components/header/title_component/title";
+import { PostContainer } from "./components/posts/postContainer";
+import { Posts } from "./components/posts/posts";
 
 export default function Home() {
   return (
     <>
       <Header_layout>
-        <h1 className="text-4xl">
-          <span className="text-textoPrincipal">Code</span>lândia
-        </h1>
-
+        <Title />
         <SearchBar />
       </Header_layout>
+
+      <PostContainer>
+        <Posts />
+      </PostContainer>
     </>
   );
 }
