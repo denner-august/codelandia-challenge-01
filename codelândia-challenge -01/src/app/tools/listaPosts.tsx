c133838b72dd824d61eaeebecb355b64d1332610
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-01-03 17:52:06 -0300

    resolvendo problema de importação

--- codelândia-challenge -01/src/app/tools/listaPosts.tsx
+++ codelândia-challenge -01/src/app/tools/listaPosts.tsx
@@ -1,4 +1,4 @@
-import { PostsCriados } from "../../../post.json";
+import { PostsCriados } from "../../../post";
 import { Posts } from "../components/posts/posts";
 import { useEffect, useState } from "react";
 import { usePesquisa } from "@/app/context/ContextZustand";
@@ -13,11 +13,13 @@ interface postCriadoProps {
 export function ListaPosts() {
   const pesquisa = usePesquisa((state) => state.pesquisa);
 
-  const [PostsSearch, UsePostsSearch] = useState(PostsCriados);
+  const [PostsSearch, UsePostsSearch] = useState(PostsCriados.PostsCriados);
 
   useEffect(() => {
     function buscaPosts() {
-      const p = PostsCriados.filter((posts) => posts.title.includes(pesquisa));
+      const p = PostsCriados.PostsCriados.filter((posts) =>
+        posts.title.includes(pesquisa)
+      );
 
       UsePostsSearch(p);
     }
